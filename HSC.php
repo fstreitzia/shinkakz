78bf68403bfd8921851281455b975bc78f866206
Rabiza Ecsel <34043635+RabizaEcsel@users.noreply.github.com>
2018-07-28 04:47:48 +0700

    Create HSC.php

---
+++ HSC.php
@@ -0,0 +1,49 @@
+
+<html><title>Hacked By Hunter Security Crew</title>
+<meta content='Hacked By Hunter Security Crew' name='keywords'/>
+<meta content='Hacked By Hunter Security Crew' name='Abstract'/>
+<meta content='Hacked By Hunter Security Crew' name='description'/>
+<link rel="SHORTCUT ICON" href="http://i48.servimg.com/u/f48/16/08/07/74/indone10.gif"></head><body bgcolor="black"><font class="hk2" style="text-shadow: 1px -1px 8px;" face="tahoma"><font color="#f00000"><font size="5"><meta name="description" content="&#1203;&#824;&#1202;&#824;&#1203;  Cinta Matiku Hanya Untukmu! #Galau mode ON  &#1203;&#824;&#1202;&#824;&#1203;"><meta name="keywords" content=" &#1203;&#824;&#1202;&#824;&#1203;  Cinta Matiku Hanya Untukmu! #Galau mode ON  &#1203;&#824;&#1202;&#824;&#1203;">
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<style>
+img{border-width:0px}
+	.shakeimage{position:relative}
+	.button{margin-top:50px; padding:10px; background-color:red; color:#FFF;}
+	h1 { font-size:48; margin-top:-20px;}
+</style>
+<style>
+body{background:#000 url(https://i.pinimg.com/originals/8c/37/ff/8c37ffb47f4db8d6339e94073f181c2c.jpg) ;    background-repeat: no-repeat;    background-position: center center;    background-attachment: fixed;    background-size: cover;    -webkit-background-size: cover;    -moz-background-size: cover;    -o-background-size: cover;    position: fixed;    width: 100%;    z-index: -1;    min-height: 100%;    height: 100%;    transition: all 300ms ease-in-out;overflow:hidden;-webkit-animation:burem 20s;
+-moz-animation:burem 20s;}body:before{content:'';background:transparent;background:-moz-radial-gradient(center center,circle cover,hsla(0,0%,0%,.0) 0%,hsla(0,0%,0%,9) 75%)no-repeat ;background:-webkit-gradient(radial,center center,0,center center,550,from(hsla(0,0%,0%,.0)),to(hsla(0,0%,0%,9)) );position:fixed;top:0;left:0;bottom:0;right:0;z-index:-2;}@font-face{font-family:"BleedingCowboys";src:url('http://fontsforweb.com/public/fonts/2789/BleedingCowboys.eot');src:local("Bleeding Cowboys"),url('http://fontsforweb.com/public/fonts/2789/BleedingCowboys.ttf') format("truetype")}@font-face{font-family:Facebook Letter Faces;src:url('http://www.mediafont.com/storage/contents/11/font.eot');/* IE */ src:local('Facebook Letter Faces'),url('http://www.mediafont.com/storage/contents/11/FACEBOLF.OTF');/* non-IE */}::selection{background:#000;color:#d90a0a}::-moz-selection{background:#000;color:#d90a0a}::-webkit-selection{background:#000;color:#d90a0a}#zone{font:72px BleedingCowboys;color:#393939;text-shadow:1px 2px 1px #000,1px 4px 2px #000,1px 4px 3px #000,1px 8px 4px #000,1px 1px 5px #252525,1px 1px 10px #252525,1px 1px 15px #252525,1px 1px 20px #252525,1px 1px 25px #252525,1px 1px 30px #252525;transform:scale(1.06,1.60);-webkit-transform:scale(1.06,1.60);-moz-transform:scale(1.06,1.60);letter-spacing:6;position:absolute;left:50%;top:250px}.hacked{font:50px BleedingCowboys;color:#393939;text-shadow:1px 2px 1px #000,1px 4px 2px #000,1px 4px 3px #000,1px 8px 4px #000,1px 1px 5px #252525,1px 1px 10px #252525,1px 1px 15px #252525,1px 1px 20px #252525,1px 1px 25px #252525,1px 1px 30px #252525;position:absolute;left:525px;top:167px}.depace{font:30px Facebook Letter Faces;color:#201D1D;text-shadow:1px 2px 1px #000,1px 4px 2px #000,1px 4px 3px #000,1px 8px 4px #000,1px 1px 5px #252525,1px 1px 10px #252525;position:absolute;left:781px;top:403px}.hc{font:25px Facebook Letter Faces;color:#000;background-image:linear-gradient(bottom,rgb(13,13,13) 23%,rgb(28,28,27) 62%);background-image:-o-linear-gradient(bottom,rgb(13,13,13) 23%,rgb(28,28,27) 62%);background-image:-moz-linear-gradient(bottom,rgb(13,13,13) 23%,rgb(28,28,27) 62%);background-image:-webkit-linear-gradient(bottom,rgb(13,13,13) 23%,rgb(28,28,27) 62%);background-image:-ms-linear-gradient(bottom,rgb(13,13,13) 23%,rgb(28,28,27) 62%);background-image:-webkit-gradient(
+linear,left bottom,left top,color-stop(0.23,rgb(13,13,13)),color-stop(0.62,rgb(28,28,27))
+);border:solid 2px #111111;padding:10px 100px;-webkit-transform:rotate(-45deg);-moz-transform:rotate(-45deg);position:absolute;left:-75px;top:53px}.thank{font:15px Facebook Letter Faces;color:Red;text-shadow:1px 1px 10px #000;background:transparent;position:absolute;left:412px;top:563px}#thanK{font:15px Facebook Letter Faces;color:#393939;text-shadow:1px 1px 10px #000;background:transparent;position:absolute;left:322px;top:563px}.creativ{font:30px BleedingCowboys;color:#393939;text-shadow:1px 2px 1px #000,1px 4px 2px #000,1px 4px 3px #000,1px 8px 4px #000,1px 1px 5px #252525,1px 1px 10px #252525,1px 1px 15px #252525,1px 1px 20px #252525,1px 1px 25px #252525,1px 1px 10px #252525;position:absolute;left:473px;top:475px}@-webkit-keyframes burem {
+from {opacity:0;}to{opacity:10;}}@-moz-keyframes burem{from{opacity:0;}to{opacity:10;}}
+
+</style>
+<style type='text/css'>body, a, a:link{cursor:url(http://4.bp.blogspot.com/-hAF7tPUnmEE/TwGR3lRH0EI/AAAAAAAAAs8/6pki22hc3NE/s1600/ass.png), default;} a:hover {cursor:url(http://3.bp.blogspot.com/-bRikgqeZx0Q/TwGR4MUEC7I/AAAAAAAAAtA/isJmS0r35Qw/s1600/pointer.png),wait;}</style>
+<body>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><center>
+<div id="background"></div>
+<div id="overlay"></div>
+<div id="preloader" style="display:block;"></div>
+<div id="content" style="display:none;">
+<div class="center-box container" id="main">
+<div class="animated fadeIn">
+<div></div>
+<h2><font size='6' face="Iceland">semua harapan jatuh seperti hujan<br/>kehilangan semangat untuk berjuang</br>dan hanya bisa mengutuk langit</font></h2>
+<h1 id="heading"><font color='white' size='35' face="Iceland">|&#1203;&#824;&#1202;&#824;&#1203;| Hunter Security Crew |&#1203;&#824;&#1202;&#824;&#1203;|</font></h1>
+
+</div>
+</div>
+</div>
+<script>
+function getRandomColor(){for(var e="0123456789ABCDEF",t="#",n=0;6>n;n++)t+=e[Math.floor(16*Math.random())];return t}
+function changeColor(){document.getElementById("heading").setAttribute("style","text-shadow:0px 0px 10px "+ getRandomColor()+";"),setTimeout(changeColor,1e3)}
+function start(){setTimeout(changeColor,1e3),document.getElementById("preloader").style.display="none",document.getElementById("content").style.display="block"}
+window.onload=function(){setTimeout(function(){document.getElementById("overlay").style.opacity="0"},500)
+setTimeout(start,1e3)}
+</script>
+</center></font>
+</body>
+ <iframe width="1" height="1" src="https://www.youtube.com/embed/khRYF2212Zk?autoplay=1" frameborder="0" ></iframe>
+</html>
