5971bfe31c185452e164c4c9cb8ceb2fc63100d4
Dolken <34043635+DolkenBrazzers@users.noreply.github.com>
2018-12-31 04:21:52 +0700

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Set the username and password of the Instagram account
-$username = ''; //usernameInstagram
-$password = ''; //passwdInstagram
-$userID = ''; //userID -> getUSERID https://smashballoon.com/instagram-feed/find-instagram-user-id/
